999f9f918790acec29d08f0dfa4afecafa7352e9
Gealisson Jorge <>
2021-11-27 21:59:01 -0300

    Adicao de script para preencher a tabela de filmes. Adicao de leitura desses filmes no index.xhtml

---
+++ src/main/resources/PersistenceSQL/sql/scripts.sql
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Gealisson
+ * Created: 27 de nov. de 2021
+ */
+INSERT INTO tbl_filme  VALUES(1,'Timothee Chalamet',12,200,'fantasia',FALSE,5,'resumo de filme','Duna');
+INSERT INTO tbl_filme  VALUES(2,'Timothee Chalamet',12,200,'fantasia',FALSE,3,'resumo de filme','Duna 2');
+INSERT INTO tbl_filme  VALUES(3,'Timothee Chalamet',12,200,'fantasia',FALSE,2,'resumo de filme','Duna 3');
+INSERT INTO tbl_filme  VALUES(4,'Stephanie Beatriz',12,200,'animacao',FALSE,4,'resumo de filme','Encanto');
+INSERT INTO tbl_filme  VALUES(5,'Angelina Jolie',12,200,'fantasia',FALSE,5,'resumo de filme','Eternos');
